9815599891715b178bdaed7a1dddbd6cb494c0ab
LENOVO <LENOVO@LAPTOP-KK5M8UOM>
2021-09-17 12:14:38 +0700

    ugiu

--- Bank/Loan/InfoLoan.cs
+++ Bank/Loan/InfoLoan.cs
@@ -12,9 +12,125 @@ namespace example.Bank.Loan
 {
     public partial class InfoLoan : Form
     {
+        int Check = 0;
+        /// <summary>
+        /// <para>[0] SELECT MemberLona  INPUT: {TeacherNo}</para>
+        /// <para>[1] SELECT LOAN INPUT: {TeacherNo} </para>
+        /// </summary>
+        private String[] SQLDefault =
+        {
+             //[0] SELECT MemberLona  INPUT: {TeacherNo}
+          "SELECT TOP(20) TeacherNo , NAME , StartAmount \r\n " +
+          "FROM( \r\n " +
+          "SELECT a.TeacherNo,CAST(c.PrefixName+''+b.Fname+''+b.Lname as NVARCHAR)AS NAME,d.StartAmount ,b.Fname \r\n " +
+          "FROM EmployeeBank.dbo.tblLoan as a  \r\n " +
+          "LEFT JOIN Personal.dbo.tblTeacherHis as b on a.TeacherNo = b.TeacherNo  \r\n " +
+          "LEFT JOIN BaseData.dbo.tblPrefix as c on b.PrefixNo = c.PrefixNo  \r\n " +
+          "LEFT JOIN EmployeeBank.dbo.tblMember as d on a.TeacherNo = d.TeacherNo \r\n " +
+          "WHERE a.TeacherNo LIKE 'T{TeacherNo}%' and a.LoanStatusNo != 4 \r\n " +
+          "GROUP BY a.TeacherNo,CAST(c.PrefixName+''+b.Fname+''+b.Lname as NVARCHAR),d.StartAmount ,Fname) AS A \r\n " +
+          "ORDER BY Fname; \r\n " +
+          " "
+          ,
+          //[1] SELECT LOAN INPUT: {TeacherNo} : 
+          "SELECT a.LoanNo , CAST(d.PrefixName + ' ' + Fname + ' ' + Lname AS NVARCHAR) \r\n " +
+          " FROM EmployeeBank.dbo.tblLoan as a  \r\n " +
+          " LEFT JOIN EmployeeBank.dbo.tblGuarantor as b on a.LoanNo = b.LoanNo  \r\n " +
+          " LEFT JOIN Personal.dbo.tblTeacherHis as c on a.TeacherNo = c.TeacherNo \r\n " +
+          " LEFT JOIN BaseData.dbo.tblPrefix as d on c.PrefixNo = d.PrefixNo \r\n " +
+          " WHERE a.TeacherNo = '{TeacherNo}' and LoanStatusNo != 4  \r\n " +
+          " GROUP BY a.LoanNo , CAST(d.PrefixName + ' ' + Fname + ' ' + Lname AS NVARCHAR) \r\n " +
+          " ORDER BY a.LoanNo  "
+
+          ,
+        };
         public InfoLoan()
         {
             InitializeComponent();
+        }
+
+        private void BSearchTeacher_Click(object sender, EventArgs e)
+        {
+            Bank.Search IN;
+            try
+            {
+
+                IN = new Bank.Search(SQLDefault[0]
+                     .Replace("{TeacherNo}", ""));
+                IN.ShowDialog();
+                TBTeacherNo.Text = Bank.Search.Return[0];
+                TBTeacherName.Text = Bank.Search.Return[1];
+                comboBox1.Enabled = true;
+                comboBox1.Items.Clear();
+                Check = 1;
+                ComboBox[] cb = new ComboBox[] { comboBox1 };
+                DataTable dt = Class.SQLConnection.InputSQLMSSQL(SQLDefault[1]
+                    .Replace("{TeacherNo}", TBTeacherNo.Text));
+                for (int x = 0; x < dt.Rows.Count; x++)
+                {
+                    for (int aa = 0; aa < cb.Length; aa++)
+                    {
+                        cb[aa].Items.Add(new example.Class.ComboBoxPayment("รายการกู้ " + (x + 1), dt.Rows[x][0].ToString()));
+                    }
+                }
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        private void TBTeacherNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (TBTeacherNo.Text.Length == 6)
+                {
+                    DataTable dt = Class.SQLConnection.InputSQLMSSQL(SQLDefault[1].Replace("{TeacherNo}", TBTeacherNo.Text));
+                    if (dt.Rows.Count != 0)
+                    {
+                        TBTeacherName.Text = dt.Rows[0][1].ToString();
+                        comboBox1.Enabled = true;
+                        comboBox1.Items.Clear();
+                        Check = 1;
+                        ComboBox[] cb = new ComboBox[] { comboBox1 };
+                        for (int x = 0; x < dt.Rows.Count; x++)
+                        {
+                            for (int aa = 0; aa < cb.Length; aa++)
+                            {
+                                cb[aa].Items.Add(new example.Class.ComboBoxPayment("รายการกู้ " + (x + 1), dt.Rows[x][0].ToString()));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("รหัสผู้ใช้ไม่ถูกต้อง", "System", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+
+            }
+            else if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back)
+            {
+                if (Check == 1)
+                {
+                    comboBox1.Items.Clear();
+                    comboBox1.SelectedIndex = -1;
+                    TBTeacherName.Text = "";
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                    textBox5.Text = "";
+                    comboBox1.Enabled = false;
+                    Check = 0;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
