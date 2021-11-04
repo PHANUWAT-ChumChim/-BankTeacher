61f0f20a488f385f7d980604a26a4d76588b082d
LENOVO <LENOVO@LAPTOP-KK5M8UOM>
2021-11-04 09:23:58 +0700

    qhdsrf

--- Bank/Add Member/MemberShip.cs
+++ Bank/Add Member/MemberShip.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -26,7 +25,8 @@ namespace BankTeacher.Bank
         public MemberShip()
         {
             InitializeComponent();
-            TBStartAmountShare.Text = BankTeacher.Bank.Menu.startAmountMin.ToString();
+            TBStartAmountShare_Reg.Text = BankTeacher.Bank.Menu.startAmountMin.ToString();
+            Relaodcancelmember();
         }
 
         //------------------------- FormSize -----------------
@@ -43,6 +43,9 @@ namespace BankTeacher.Bank
         /// <para>[1] SELECT Member  INPUT:{Text} </para>
         /// <para>[2]  Select Detail Memner INPUT: {TeacherNo} </para>
         /// <para>[3] INSERT Member To Member  Bill BillDetail  INPUT: {TeacherNo} {TeacherNoAddBy} {StartAmount} {Mount} {Year}  </para>
+        /// <para>[4] Change Status Member INPUT: {TeacherNoAddBy} {TeacherNo} {Note} {DocStatusNo} {DocUploadPath} {Status} {TeacherNo}</para>
+        /// <para>[5] SELECT MEMBER INPUT: {Text}</para>
+        /// <para>[6] Select MemberResignation INPUT: {Date} </para>
         /// </summary>
         private String[] SQLDefault = new String[]
         {
@@ -113,8 +116,39 @@ namespace BankTeacher.Bank
           "END; \r\n " +
           " "
           ,
+          
+        //[4] Change Status Member INPUT: {TeacherNoAddBy} {TeacherNo} {Note} {DocStatusNo} {DocUploadPath} {Status}
+            "INSERT INTO EmployeeBank.dbo.tblMemberResignation (TeacherNoAddBy,TeacherNo,Date,Note,DocStatusNo,DocUploadPath) \r\n " +
+            "VALUES ('{TeacherNoAddBy}','{TeacherNo}',CURRENT_TIMESTAMP,'{Note}','{DocStatusNo}','{DocUploadPath}'); \r\n " +
+            " \r\n " +
+            "UPDATE EmployeeBank.dbo.tblMember \r\n " +
+            "SET MemberStatusNo = '{Status}' \r\n " +
+            "WHERE TeacherNo = '{TeacherNo}' "
+        ,
+        //[5] SELECT MEMBER INPUT: {Text}
+            "SELECT TOP(20) a.TeacherNo , CAST(c.PrefixName+' '+[Fname] +' '+ [Lname] as NVARCHAR)AS Name, e.SavingAmount,    \r\n " +
+            "b.TeacherLicenseNo,b.IdNo AS IDNo,b.TelMobile ,a.StartAmount,CAST(d.MemberStatusName as nvarchar) AS UserStatususing    \r\n " +
+            "FROM EmployeeBank.dbo.tblMember as a    \r\n " +
+            "LEFT JOIN Personal.dbo.tblTeacherHis as b ON a.TeacherNo = b.TeacherNo    \r\n " +
+            "LEFT JOIN BaseData.dbo.tblPrefix as c ON c.PrefixNo = b.PrefixNo   \r\n " +
+            "INNER JOIN EmployeeBank.dbo.tblMemberStatus as d on a.MemberStatusNo = d.MemberStatusNo  \r\n " +
+            "LEFT JOIN EmployeeBank.dbo.tblShare as e on a.TeacherNo = e.TeacherNo \r\n " +
+            "WHERE a.MemberStatusNo = 1 and a.TeacherNo LIKE '%{Text}%'  or CAST(c.PrefixName+' '+[Fname] +' '+ [Lname] as NVARCHAR) LIKE '%{Text}%'   and a.MemberStatusNo = 1         \r\n " +
+            "GROUP BY a.TeacherNo , CAST(c.PrefixName+' '+[Fname] +' '+ [Lname] as NVARCHAR), e.SavingAmount,    \r\n " +
+            "b.TeacherLicenseNo,b.IdNo ,b.TelMobile ,a.StartAmount,CAST(d.MemberStatusName as nvarchar)   \r\n " +
+            "ORDER BY a.TeacherNo; "
+            ,
+            //[6] Select MemberResignation INPUT: {Date}
+           "SELECT Date,b.TeacherNo , CAST (ISNULL(c.PrefixName , '')+' '+Fname + ' ' + Lname as nvarchar),Note \r\n " +
+          "FROM EmployeeBank.dbo.tblMemberResignation as a \r\n " +
+          "LEFT JOIN Personal.dbo.tblTeacherHis as b on a.TeacherNo = b.TeacherNo \r\n " +
+          "LEFT JOIN BaseData.dbo.tblPrefix as c on b.PrefixNo = c.PrefixNo \r\n " +
+          "WHERE CAST(CAST(DATE  as  date)as nvarchar(50))  LIKE  '{Date}%'"
+           ,
 
-        };
+
+
+    };
 
         private void BSearchTeacher_Click(object sender, EventArgs e)
         {
@@ -124,8 +158,8 @@ namespace BankTeacher.Bank
                 IN.ShowDialog();
                 if (Search.Return[0] != "")
                 {
-                    TBTeacherNo.Text = Search.Return[0];
-                    TBTeacherName.Text = Search.Return[1];
+                    TBTeacherNo_Reg.Text = Search.Return[0];
+                    TBTeacherName_Reg.Text = Search.Return[1];
                     TBTeacherNo_KeyDown(sender, new KeyEventArgs(Keys.Enter));
                 }
 
@@ -139,13 +173,13 @@ namespace BankTeacher.Bank
         // Available values|  SQLDefault[1] / TB /
         private void BSave_Click_1(object sender, EventArgs e)
         {
-            int AmountShare = Convert.ToInt32(TBStartAmountShare.Text);
-            if(AmountShare.ToString() == "" || AmountShare == 0)
+            int AmountShare = Convert.ToInt32(TBStartAmountShare_Reg.Text);
+            if (AmountShare.ToString() == "" || AmountShare == 0)
             {
                 AmountShare = BankTeacher.Bank.Menu.startAmountMin;
             }
-            DataTable dt = Class.SQLConnection.InputSQLMSSQL(SQLDefault[1].Replace("{TeacherNo}", TBTeacherNo.Text));
-            if (TBTeacherName.Text != "")
+            DataTable dt = Class.SQLConnection.InputSQLMSSQL(SQLDefault[1].Replace("{TeacherNo}", TBTeacherNo_Reg.Text));
+            if (TBTeacherName_Reg.Text != "")
             {
                 if (AmountShare >= BankTeacher.Bank.Menu.startAmountMin && AmountShare <= BankTeacher.Bank.Menu.startAmountMax)
                 {
@@ -155,14 +189,12 @@ namespace BankTeacher.Bank
                         DialogResult dialogResult = MessageBox.Show("ยืนยันการสมัคร", "สมัครสมาชิก", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
                         if (dialogResult == DialogResult.Yes)
                         {
-                            Class.SQLConnection.InputSQLMSSQL(SQLDefault[3].Replace("{TeacherNo}", TBTeacherNo.Text)
+                            Class.SQLConnection.InputSQLMSSQL(SQLDefault[3].Replace("{TeacherNo}", TBTeacherNo_Reg.Text)
                             .Replace("{TeacherNoAddBy}", BankTeacher.Class.UserInfo.TeacherNo)
                             .Replace("{StartAmount}", AmountShare.ToString())
                             .Replace("{Month}", BankTeacher.Bank.Menu.Date[1])
                             .Replace("{Year}", BankTeacher.Bank.Menu.Date[0]));
                             MessageBox.Show("สมัครเสร็จสิ้น", "สมัครสมาชิก", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            TBTeacherNo.Clear();
-                            TBTeacherName.Clear(); 
                         }
                         else
                         {
@@ -221,12 +253,6 @@ namespace BankTeacher.Bank
 
             }
         }
-        //----------------------- End code -------------------- ////////
-
-        private void membership_Load(object sender, EventArgs e)
-        {
-        }
-
 
         // ใช้งานเเน่นอนไม่ต้องลบนะครับ
         private void T()
@@ -262,50 +288,57 @@ namespace BankTeacher.Bank
         //----------------------- Printf -------------------- ////////
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-            BankTeacher.Class.Print.PrintPreviewDialog.PrintMember(e,SQLDefault[2],BankTeacher.Bank.Menu.Date[2],BankTeacher.Bank.Menu.Monthname,(Convert.ToInt32(BankTeacher.Bank.Menu.Date[0]) + 543).ToString(),TBTeacherNo.Text,TBStartAmountShare.Text);
+            BankTeacher.Class.Print.PrintPreviewDialog.PrintMember(e, SQLDefault[2], BankTeacher.Bank.Menu.Date[2], BankTeacher.Bank.Menu.Monthname, (Convert.ToInt32(BankTeacher.Bank.Menu.Date[0]) + 543).ToString(), TBTeacherNo_Reg.Text, TBStartAmountShare_Reg.Text);
         }
 
         private void BTOpenfile_Click(object sender, EventArgs e)
         {
-            if (StatusBoxFile == 0)
+            if (TBTeacherNo_Reg.Text.Length == 6)
             {
-
-                try
+                if (StatusBoxFile == 0)
                 {
-                    OpenFileDialog dialog = new OpenFileDialog();
-                    dialog.Filter = "pdf files(*.pdf)|*.pdf";
-                    if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                    {
-                        imgeLocation = dialog.FileName;
-                    }
-                    if (imgeLocation != "")
-                    {
-                        BTOpenfile.Text = "ส่งไฟล์";
-                        StatusBoxFile = 1;
-                        //label6.Text = "Scan(  พบไฟล์  )";
-                    }
 
+                    try
+                    {
+                        OpenFileDialog dialog = new OpenFileDialog();
+                        dialog.Filter = "pdf files(*.pdf)|*.pdf";
+                        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        {
+                            imgeLocation = dialog.FileName;
+                        }
+                        if (imgeLocation != "")
+                        {
+                            BTOpenfile_Reg.Text = "ส่งไฟล์";
+                            StatusBoxFile = 1;
+                            LScan_Reg.Text = "Scan(  พบไฟล์  )";
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
-                catch
+                else if (StatusBoxFile == 1)
                 {
-                    MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    var smb = new SmbFileContainer("Member");
+                    if (smb.IsValidConnection())
+                    {
+                        String Return = smb.SendFile(imgeLocation, "Member" + TBTeacherNo_Reg.Text + ".pdf");
+                        MessageBox.Show(Return, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        StatusBoxFile = 0;
+                        BTOpenfile_Reg.Text = "เปิดไฟล์";
+                        imgeLocation = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("ไม่สามารถสร้างไฟล์ในที่นั้นได้", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
                 }
             }
-            else if (StatusBoxFile == 1)
+            else
             {
-                var smb = new SmbFileContainer("Member");
-                if (smb.IsValidConnection())
-                {
-                    String Return = smb.SendFile(imgeLocation, "Member" + TBTeacherNo.Text + ".pdf");
-                    MessageBox.Show(Return, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    StatusBoxFile = 0;
-                    BTOpenfile.Text = "เปิดไฟล์";
-                    imgeLocation = "";
-                }
-                else
-                {
-                    MessageBox.Show("ไม่สามารถสร้างไฟล์ในที่นั้นได้", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
+                MessageBox.Show("กรุณากรอก รหัสผู้ใช้ก่อน", "ระบบ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
@@ -318,13 +351,12 @@ namespace BankTeacher.Bank
         {
             if (e.KeyCode == Keys.Enter)
             {
-                if(TBTeacherNo.Text.Length == 6)
+                if (TBTeacherNo_Reg.Text.Length == 6)
                 {
                     try
                     {
-                         DataSet ds = Class.SQLConnection.InputSQLMSSQLDS(SQLDefault[1].Replace("{Text}", TBTeacherNo.Text));
-                        TBTeacherName.Text = ds.Tables[0].Rows[0][1].ToString();
-                        TBIDNo.Text = "รอใส่จ้าาา";
+                        DataSet ds = Class.SQLConnection.InputSQLMSSQLDS(SQLDefault[1].Replace("{Text}", TBTeacherNo_Reg.Text));
+                        TBTeacherName_Reg.Text = ds.Tables[0].Rows[0][1].ToString();
                         Check = 1;
 
                     }
@@ -332,12 +364,11 @@ namespace BankTeacher.Bank
                     {
                         Console.WriteLine(ex);
                     }
-                } 
+                }
             }
             else if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back && Check == 1)
             {
-                TBIDNo.Text = "";
-                TBTeacherName.Text = "";
+                TBTeacherName_Reg.Text = "";
                 Check = 0;
             }
         }
@@ -345,34 +376,167 @@ namespace BankTeacher.Bank
         private void BTdeletefile_Click(object sender, EventArgs e)
         {
             StatusBoxFile = 0;
-            BTOpenfile.Text = "เปิดไฟล์";
+            BTOpenfile_Reg.Text = "เปิดไฟล์";
             imgeLocation = "";
         }
 
-        private void TBStartAmountShare_TextChanged(object sender, EventArgs e)
+        private void BSearch_Cancel_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Bank.Search IN = new Bank.Search(SQLDefault[5]);
+                IN.ShowDialog();
+                if (Bank.Search.Return[0] != "")
+                {
+                    TBTeacherNO_Cancel.Text = Bank.Search.Return[0];
+                    TBTeacherNO_Cancel_KeyDown(new object(), new KeyEventArgs(Keys.Enter));
+                }
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x);
+            }
         }
 
-        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        private void TBTeacherNO_Cancel_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (TBTeacherNO_Cancel.Text.Length == 6)
+                {
+                    try
+                    {
+                        DataSet ds = Class.SQLConnection.InputSQLMSSQLDS(SQLDefault[5].Replace("{Text}", TBTeacherNO_Cancel.Text));
+                        TBTeacherName_Cancel.Text = ds.Tables[0].Rows[0][1].ToString();
+                        Check = 1;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+            else if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back && Check == 1)
+            {
+                TBTeacherName_Cancel.Text = "";
+                Check = 0;
+            }
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void BSave_Cancel_Click(object sender, EventArgs e)
         {
-
+            if (TBTeacherNO_Cancel.Text != "")
+            {
+                Class.SQLConnection.InputSQLMSSQLDS(SQLDefault[4]
+                    .Replace("{TeacherNoAddBy}", Class.UserInfo.TeacherNo)
+                    .Replace("{TeacherNo}", TBTeacherNO_Cancel.Text)
+                    .Replace("{Note}", TBNote_Cancel.Text)
+                    .Replace("{DocStatusNo}", "2")
+                    .Replace("{DocUploadPath}", "")
+                    .Replace("{Status}", "2"));
+                MessageBox.Show("ยกเลิกผู้ใช้เรียบร้อย", "System", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                imgeLocation = "";
+            }
+            else
+            {
+                MessageBox.Show("กรุณาใส่รหัสให้ถูกต้อง", "เตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
-        //----------------------- End code -------------------- ////////
 
+        private void BOpenFile_Cancel_Click(object sender, EventArgs e)
+        {
+            if (TBTeacherNO_Cancel.Text.Length == 6)
+            {
+                if (StatusBoxFile == 0)
+                {
 
+                    try
+                    {
+                        OpenFileDialog dialog = new OpenFileDialog();
+                        dialog.Filter = "pdf files(*.pdf)|*.pdf";
+                        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        {
+                            imgeLocation = dialog.FileName;
+                        }
+                        if (imgeLocation != "")
+                        {
+                            LScan_Cancel.Text = "Scan( พบไฟล์ )";
+                            BOpenFile_Cancel.Text = "ส่งไฟล์";
+                            StatusBoxFile = 1;
+                        }
 
+                    }
+                    catch
+                    {
+                        MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else if (StatusBoxFile == 1)
+                {
+                    var smb = new SmbFileContainer("CancelMember");
+                    if (smb.IsValidConnection())
+                    {
+                        String Return = smb.SendFile(imgeLocation, "CancelMember" + TBTeacherNO_Cancel.Text + ".pdf");
+                        if (Return.Contains("Fail"))
+                        {
+                            MessageBox.Show(Return, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        else
+                        {
+                            MessageBox.Show(Return, "System", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        StatusBoxFile = 0;
+                        BOpenFile_Cancel.Text = "เปิดไฟล์";
+                        LScan_Cancel.Text = "Scan(  ไม่พบ  )";
+                        imgeLocation = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("ไม่สามารถสร้างไฟล์ในที่นั้นได้", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("กรุณากรอก รหัสผู้ใช้ก่อน", "ระบบ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
 
-        // ------------------------ not working --------------
-        //----------------------- End code -------------------- ////////
+        private void BDeleteFile_Cancel_Click(object sender, EventArgs e)
+        {
+            imgeLocation = "";
+            BOpenFile_Cancel.Text = "เปิดไฟล์";
+            LScan_Cancel.Text = "Scan(  ไม่พบ  )";
+            StatusBoxFile = 0;
+        }
+        private void Relaodcancelmember()
+        {
+            int Year = Convert.ToInt32(BankTeacher.Bank.Menu.Date[0]);
+            for (int a = 0; a < 5; a++)
+            {
+                if (Class.SQLConnection.InputSQLMSSQL(SQLDefault[6]
+                    .Replace("{Date}", Year.ToString())).Rows.Count != 0)
+                    CBYear_HistoryCancel.Items.Add(Year);
+                Year--;
+            }
+        }
 
-
+        private void CBYear_HistoryCancel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = Class.SQLConnection.InputSQLMSSQL(SQLDefault[6].Replace("{Date}", CBYear_HistoryCancel.Text));
+            if (dt.Rows.Count != 0)
+            {
+                for (int x = 0; x < dt.Rows.Count; x++)
+                    DGV_HistoryCancel.Rows.Add((Convert.ToDateTime(dt.Rows[x][0].ToString())).ToString("yyyy-MM-dd"), dt.Rows[x][1].ToString(), dt.Rows[x][2].ToString(), dt.Rows[x][3].ToString());
+                for (int x = 0; x < dt.Rows.Count; x++)
+                {
+                    if (x % 2 == 1)
+                    {
+                        DGV_HistoryCancel.Rows[x].DefaultCellStyle.BackColor = Color.AliceBlue;
+                    }
+                }
+            }
+        }
     }
 }
-            
-            
-       
-          
