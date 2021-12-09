05d0fce0f5dfd1abecf397c7682d0eb057b064a3
LENOVO <LENOVO@LAPTOP-KK5M8UOM>
2021-12-09 09:24:04 +0700

    Update Icon

--- Bank/Menu.cs
+++ Bank/Menu.cs
@@ -169,7 +169,6 @@ namespace BankTeacher.Bank
         {
             Bank.ReportEpensesAll reportEpensesall = new Bank.ReportEpensesAll();
             CloseFrom(reportEpensesall);
-            ///ljsbdkawbfjklanfljkesbflka
         }
 
 
