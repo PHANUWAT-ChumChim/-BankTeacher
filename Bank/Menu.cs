283eb2b62fff0380ca73f98c7762fcbd94271370
LENOVO <LENOVO@LAPTOP-KK5M8UOM>
2021-12-09 09:25:10 +0700

    Update Icon

--- Bank/Menu.cs
+++ Bank/Menu.cs
@@ -115,6 +115,7 @@ namespace BankTeacher.Bank
         {
             Bank.AmountOff AmountOff = new Bank.AmountOff();
             CloseFrom(AmountOff);
+            //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
         }
 
         private void aaToolStripMenuItem_Click(object sender, EventArgs e)
