ee20075e3c3d2dc9d75867199c0153714ad531c1
LENOVO <LENOVO@LAPTOP-KK5M8UOM>
2022-02-02 13:31:00 +0700

    Update Dididend log V3

--- Bank/log/Dividend_log.Designer.cs
+++ Bank/log/Dividend_log.Designer.cs
@@ -29,12 +29,111 @@ namespace BankTeacher.Bank.log
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(61, 21);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 40);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(33, 79);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(819, 396);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ปี";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "รหัส";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "ชื่อ - สกุล";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "วันที่ทำรายการ";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 230;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "สถาณะ";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 180;
+            // 
+            // Dividend_log
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(888, 497);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("TH Sarabun New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.Name = "Dividend_log";
             this.Text = "Devidend_log";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Bank/log/Dividend_log.cs
+++ Bank/log/Dividend_log.cs
@@ -12,9 +12,68 @@ namespace BankTeacher.Bank.log
 {
     public partial class Dividend_log : Form
     {
+        /// <summary> 
+        /// SQLDefault 
+        /// <para>[0] Select Year INPUT:  </para> 
+        /// <para>[0] Select Detail In Year INPUT: {Year}</para>
+        /// </summary> 
+        private String[] SQLDefault = new String[]
+         { 
+           //[0] Select Year INPUT: 
+           "SELECT Year \r\n " +
+          "FROM EmployeeBank.dbo.tblDividend \r\n " +
+          "GROUP BY Year \r\n " +
+          "ORDER BY Min(Year)"
+           ,
+           //[0] Select Detail In Year INPUT: {Year}
+           "SELECT a.TeacherNo , CAST(ISNULL(b.PrefixName,'')+a.Fname + ' '+ Lname as nvarchar(255)),CAST(c.DateAdd as date) , Cancel \r\n " +
+          "FROM Personal.dbo.tblTeacherHis as a \r\n " +
+          "LEFT JOIN BaseData.dbo.tblPrefix as b on a.PrefixNo = b.PrefixNo \r\n " +
+          "LEFT JOIN EmployeeBank.dbo.tblDividend as c on a.TeacherNo = c.TeacherAddby \r\n " +
+          "WHERE c.Year = 2022 \r\n " +
+          "ORDER BY c.DateAdd"
+           ,
+
+         };
         public Dividend_log()
         {
             InitializeComponent();
+            DataTable dt = BankTeacher.Class.SQLConnection.InputSQLMSSQL(SQLDefault[0]);
+            if(dt.Rows.Count != 0)
+            {
+                for(int x  = 0; x < dt.Rows.Count; x++)
+                {
+                    comboBox1.Items.Add(dt.Rows[x][0].ToString());
+                }
+                comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+            }
+            else
+            {
+                comboBox1.Enabled = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            if (comboBox1.SelectedIndex != -1)
+            {
+                if(comboBox1.Text != "")
+                {
+                    DataTable dt = BankTeacher.Class.SQLConnection.InputSQLMSSQL(SQLDefault[1]
+                        .Replace("{Year}", comboBox1.Items[comboBox1.SelectedIndex].ToString()));
+                    if(dt.Rows.Count != 0)
+                    {
+                        for(int x = 0; x < dt.Rows.Count; x++)
+                        {
+                            String Status = "ยกเลิก";
+                            if (dt.Rows[x][3].ToString() == "1")
+                                Status = "ใช้งาน";
+                            dataGridView1.Rows.Add(dt.Rows[x][0].ToString(),dt.Rows[x][1].ToString(),dt.Rows[x][2].ToString(),Status);
+                        }
+                    }
+                }
+            }
         }
     }
 }
