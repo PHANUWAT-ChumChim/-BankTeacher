2921596195734be28029c7034f813f16f82ea2d0
LENOVO <LENOVO@LAPTOP-KK5M8UOM>
2022-02-02 13:17:54 +0700

    Fixed Form is null

---
+++ Bank/log/Dividend_log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankTeacher.Bank.log
+{
+    public partial class Dividend_log : Form
+    {
+        public Dividend_log()
+        {
+            InitializeComponent();
+        }
+    }
+}
